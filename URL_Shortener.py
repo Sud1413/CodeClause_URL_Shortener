e31fc469990128e2e78e38b29f547ae8e4994470
Sud1413 <91454007+Sud1413@users.noreply.github.com>
2023-05-18 17:23:21 +0530

    Add files via upload

---
+++ URL_Shortener.py
@@ -0,0 +1,15 @@
+import pyshorteners
+import validators
+long_url = input("Enter the URL to shorten: ")
+# type_tiny = pyshorteners.Shortener()
+# short_url = type_tiny.tinyurl.short(long_url)
+# print("The Shortened URL is: " + short_url)
+
+
+if not validators.url(long_url):
+     print ("This input is invalid")
+
+else:
+     type_tiny = pyshorteners.Shortener()
+     short_url = type_tiny.tinyurl.short(long_url)
+     print("The Shortened URL is: " + short_url)
